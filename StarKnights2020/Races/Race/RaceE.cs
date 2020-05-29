9cd2afa5208cd6994993be567e178ebeb478e9b8
Antony Robert Wells <antonyrobwells@gmail.com>
2020-05-29 14:09:08 +0100

    race info progress
    Added E race description.

--- StarKnights2020/Races/Race/RaceE.cs
+++ StarKnights2020/Races/Race/RaceE.cs
@@ -15,6 +15,13 @@ namespace Knights.Races.Race
             Name = "E";
             LoadAssets();
 
+            AddInfo("E as they are known, are a group of life forms");
+            AddInfo("with an advanced set of technologies and spacecraft.");
+            AddInfo("");
+            AddInfo("They are the closest civilization to the Human race.");
+            AddInfo("");
+            AddInfo("Although peaceful, they will fight to protect their future.");
+
         }
 
     }
